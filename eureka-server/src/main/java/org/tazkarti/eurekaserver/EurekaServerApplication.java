42ab74da991b018a92a7af64dc450aea0c146178
Sersawy <65075626+Abdelrhmansersawy@users.noreply.github.com>
2025-07-09 03:24:19 +0300

    Issue #5: implement google checkstyle configuration for eureka serve

--- eureka-server/src/main/java/org/tazkarti/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/org/tazkarti/eurekaserver/EurekaServerApplication.java
@@ -2,14 +2,18 @@ package org.tazkarti.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.*;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * Main application class for the Eureka Server.
+ * This class bootstraps the Spring Boot application and enables the Eureka Server.
+ */
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(EurekaServerApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(EurekaServerApplication.class, args);
+  }
 
 }
